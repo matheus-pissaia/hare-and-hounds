d52e7fe88d34b1bd7788f583ddbd8d7aaf6374a5
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-10-02 02:33:38 -0300

    refactor: piece model

--- src/models/Piece.py
+++ src/models/Piece.py
@@ -1,21 +1,29 @@
+import typing
 from enums.Animal import Animal
+
+# Avoids circular imports
+if typing.TYPE_CHECKING:
+    from models.Position import Position
 
 
 class Piece:
     __animal: Animal
-    __position: tuple[int, int]  # TODO use position class
+    __position: "Position"
 
-    def __init__(self, animal: Animal):
+    def __init__(self, animal: Animal, position: "Position"):
         self.__animal = animal
+        self.__position = position
+        self.__position.piece = self
 
     @property
-    def animal(self):
+    def animal(self) -> Animal:
         return self.__animal
 
     @property
-    def position(self):
+    def position(self) -> "Position":
         return self.__position
 
     @position.setter
-    def position(self, position: tuple[int, int]):
-        self.__position = position
+    def position(self, new_position: "Position"):
+        self.position.piece = None
+        self.__position = new_position
